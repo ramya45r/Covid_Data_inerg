7d64fb1f19d68435a51a86ee3551df427557cd26
Ramya Mol R <ramya45r@gmail.com>
2024-08-13 22:05:37 +0530

    piechart and graph completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchCovidData } from './redux/covidSlice';
-import Filter from './components/Filter';
-import Summary from './components/Summary';
-import PieChart from './components/PieChart';
-import { AppDispatch } from './redux/store';
-import Chart from './components/Chart';
-import Map from './components/Map';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCovidData } from "./redux/covidSlice";
+import Filter from "./components/Filter";
+import Summary from "./components/Summary";
+import PieChart from "./components/PieChart";
+import { AppDispatch } from "./redux/store";
+import Chart from "./components/Chart";
+import Map from "./components/Map";
 
 const App: React.FC = () => {
-  const dispatch = useDispatch<AppDispatch>(); 
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(fetchCovidData());
@@ -17,21 +17,25 @@ const App: React.FC = () => {
 
   return (
     <div>
-    <h1  className='maintext'>COVID India</h1>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start' }}>
-      <div>
-      <Filter />
-    <Summary />
-
+      <h1 className="maintext">COVID India</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+        }}
+      >
+        <div>
+          <Filter />
+          <Summary />
+        </div>
+        <PieChart />
       </div>
-      <PieChart />
-    </div>
 
-    <Chart/>
-    <h1  className='maintext'>Map View</h1>
-    <Map/>
-  </div>
-  
+      <Chart />
+      <h1 className="maintext">Map View</h1>
+      <Map />
+    </div>
   );
 };
 
